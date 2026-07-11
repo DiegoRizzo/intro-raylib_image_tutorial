c7af0175d648e2602702c895bcbe28736c13f8f3
diegorizzo <diegochunr@hotmail.com>
2026-07-11 17:28:26 -0600

    Modificación de main.rs para colorear más pixeles de la imagen

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,156 @@ fn main() {
         Color::BLACK
     );
 
-    let pixel_position_x = 100;
-    let pixel_position_y = 100;
+    let pixel1_position_x = 50;
+    let pixel1_position_y = 50;
 
     new_image.draw_pixel(
-        pixel_position_x,
-        pixel_position_y,
+        pixel1_position_x,
+        pixel1_position_y,
+        Color::WHITE
+    );
+
+    let pixel2_position_x = 150;
+    let pixel2_position_y = 150;
+
+    new_image.draw_pixel(
+        pixel2_position_x,
+        pixel2_position_y,
+        Color::WHITE
+    );
+
+    let pixel3_position_x = 250;
+    let pixel3_position_y = 250;
+
+    new_image.draw_pixel(
+        pixel3_position_x,
+        pixel3_position_y,
+        Color::WHITE
+    );
+
+    let pixel4_position_x = 350;
+    let pixel4_position_y = 350;
+
+    new_image.draw_pixel(
+        pixel4_position_x,
+        pixel4_position_y,
+        Color::WHITE
+    );
+
+    let pixel5_position_x = 450;
+    let pixel5_position_y = 450;
+
+    new_image.draw_pixel(
+        pixel5_position_x,
+        pixel5_position_y,
+        Color::WHITE
+    );
+
+    let pixel6_position_x = 450;
+    let pixel6_position_y = 50;
+
+    new_image.draw_pixel(
+        pixel6_position_x,
+        pixel6_position_y,
+        Color::WHITE
+    );
+
+    let pixel7_position_x = 350;
+    let pixel7_position_y = 150;
+
+    new_image.draw_pixel(
+        pixel7_position_x,
+        pixel7_position_y,
+        Color::WHITE
+    );
+
+    let pixel8_position_x = 150;
+    let pixel8_position_y = 350;
+
+    new_image.draw_pixel(
+        pixel8_position_x,
+        pixel8_position_y,
+        Color::WHITE
+    );
+
+    let pixel9_position_x = 50;
+    let pixel9_position_y = 450;
+
+    new_image.draw_pixel(
+        pixel9_position_x,
+        pixel9_position_y,
+        Color::WHITE
+    );
+
+    let pixel10_position_x = 100;
+    let pixel10_position_y = 100;
+
+    new_image.draw_pixel(
+        pixel10_position_x,
+        pixel10_position_y,
+        Color::WHITE
+    );
+
+    let pixel11_position_x = 200;
+    let pixel11_position_y = 200;
+
+    new_image.draw_pixel(
+        pixel11_position_x,
+        pixel11_position_y,
+        Color::WHITE
+    );
+
+    let pixel12_position_x = 300;
+    let pixel12_position_y = 300;
+
+    new_image.draw_pixel(
+        pixel12_position_x,
+        pixel12_position_y,
+        Color::WHITE
+    );
+
+    let pixel13_position_x = 400;
+    let pixel13_position_y = 400;
+
+    new_image.draw_pixel(
+        pixel13_position_x,
+        pixel13_position_y,
+        Color::WHITE
+    );
+
+    let pixel14_position_x = 400;
+    let pixel14_position_y = 100;
+
+    new_image.draw_pixel(
+        pixel14_position_x,
+        pixel14_position_y,
+        Color::WHITE
+    );
+
+    let pixel15_position_x = 300;
+    let pixel15_position_y = 200;
+
+    new_image.draw_pixel(
+        pixel15_position_x,
+        pixel15_position_y,
+        Color::WHITE
+    );
+
+    let pixel16_position_x = 200;
+    let pixel16_position_y = 300;
+
+    new_image.draw_pixel(
+        pixel16_position_x,
+        pixel16_position_y,
+        Color::WHITE
+    );
+
+    let pixel17_position_x = 100;
+    let pixel17_position_y = 400;
+
+    new_image.draw_pixel(
+        pixel17_position_x,
+        pixel17_position_y,
         Color::WHITE
     );
 
