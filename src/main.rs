40c5c3bff56188a9f138c11a5111b4bf6034171d
diegorizzo <diegochunr@hotmail.com>
2026-07-11 16:52:43 -0600

    Creación de proyecto de Rust con Cargo

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use raylib::prelude::*;
+
+fn main() {
+    let image_width = 500;
+    let image_height = 500;
+
+    let mut new_image = Image::gen_image_color(
+        image_width,
+        image_height,
+        Color::BLACK
+    );
+
+    let pixel_position_x = 100;
+    let pixel_position_y = 100;
+
+    new_image.draw_pixel(
+        pixel_position_x,
+        pixel_position_y,
+        Color::WHITE
+    );
+
+    let output_file_name = "my_first_image.png";
+
+    new_image.export(output_file_name);
+
+ }
